d6a062a5d8f9bf1a2485bd18312665782e1b9ff2
minivesre <florendestyanindy@gmail.com>
2024-08-28 15:34:34 +0700

    initial commit

---
+++ example-app/routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\articelcontroller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResource('articles', articelcontroller::class);
